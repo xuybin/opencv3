7bc07357a4f5a5422cb0377cda62090ba8803026
xuybin <xuybin@qq.com>
2018-12-10 17:08:52 +0800

    1.0.2

--- library/src/main/cpp/HelloNDKforJava.cpp
+++ library/src/main/cpp/HelloNDKforJava.cpp
@@ -4,7 +4,7 @@
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_github_xuybin_opencv3_HelloNDKforJava_sayHelloJava(
         JNIEnv *env,
-        jclass  ) {
+        jclass /* this class*/ ) {
     std::string hello = "Hello Java from C++";
     return env->NewStringUTF(hello.c_str());
 }

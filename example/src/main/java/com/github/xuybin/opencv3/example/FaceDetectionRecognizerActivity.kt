bc83a62488402c9d5771ffdefefe8760391c7ee1
Xu YongBin <xuybin@qq.com>
2018-12-15 14:25:05 +0800

    reset

---
+++ example/src/main/java/com/github/xuybin/opencv3/example/FaceDetectionRecognizerActivity.kt
@@ -0,0 +1,12 @@
+package com.github.xuybin.opencv3.example
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FaceDetectionRecognizerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_face_detection_recognizer)
+    }
+}
--- example/src/main/java/com/github/xuybin/opencv3/example/MainActivity.kt
+++ example/src/main/java/com/github/xuybin/opencv3/example/MainActivity.kt
@@ -11,10 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 15格拼图
         btn_15_puzzle.setOnClickListener {
             startActivity(Intent(this,Puzzle15Activity::class.java))
         }
+        // 相机校准
+        btn_camera_calibration.setOnClickListener {
 
+        }
+        // 彩色斑点检测
+        btn_color_blob_detection.setOnClickListener{
+
+        }
+
+        // NDK调用测试
         btn_hello_ndk.setOnClickListener {
            if (btn_hello_ndk.text.toString().contains("java",true)) {
                btn_hello_ndk.text=HelloNDKforKotlin().sayHelloKotlin()
@@ -22,13 +32,37 @@ class MainActivity : AppCompatActivity() {
                btn_hello_ndk.text=HelloNDKforJava.sayHelloJava()
            }
         }
+
+        // 人脸面部检测
         btn_face_detection.setOnClickListener {
             startActivity(Intent(this,FdActivity::class.java))
         }
+
+        // 人脸面部检测和识别
         btn_face_detection_recognizer.setOnClickListener {
-            startActivity(Intent(this,FdActivity1::class.java))
+            startActivity(Intent(this,FaceDetectionRecognizerActivity::class.java))
         }
 
+
+        // 图像处理
+        btn_image_manipulations.setOnClickListener {
+
+        }
+
+        //教程1相机预览
+        btn_tutorial_1_camerapreview.setOnClickListener {
+
+        }
+
+        //教程2混合处理
+        btn_tutorial_2_mixedprocessing.setOnClickListener {
+
+        }
+
+        //教程3相机控制
+        btn_tutorial_3_cameracontrol.setOnClickListener {
+
+        }
     }
 
 }
